287e0d1c214228525cf9a7948cc5cf5edcefcb0c
chrisaddington96 <47895366+chrisaddington96@users.noreply.github.com>
2021-09-04 17:03:24 -0400

    Update k-NN.py

--- k-NN.py
+++ k-NN.py
@@ -14,6 +14,7 @@ from decimal import Decimal
 from random import seed
 from random import randrange
 from csv import reader
+import os
 
 # Dataset for testing purporses
 '''
@@ -90,9 +91,11 @@ Run from csv files
 '''
 
 def load_csv(filename):
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
     # Load dataset from csv file
     dataset = list()
-    with open(filename, 'r') as file:
+    with open(os.path.join(__location__, filename)) as file:
         csv_reader = reader(file)
         for row in csv_reader:
             if not row:
@@ -143,7 +146,64 @@ def cross_validation_split(dataset, n_folds):
             fold.append(dataset_copy.pop(index))
         dataset_split.append(fold)
     return dataset_split
+
+def accuracy_metric(actual, predicted):
+    # Calculate accuracy percentage
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    # Evaluate the algorithm using cross validation split
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+def k_nearest_neighbors(train, test, num_neighbors=3, distance_func=2):
+    #kNN algorithm
+    predictions = list()
+    for row in test:
+        output = predict_classification(train, row, num_neighbors, distance_func)
+        predictions.append(output)
+    return predictions
+
+# Global vars for testing
+seed(1)
+n_folds = 5
+num_neighbors = 5
+distance_func = 2
+
+def kNN_driver(filename='iris.csv'):
+    # Load data set
+    dataset = load_csv(filename)
     
+    # Change string to float for each entry
+    for i in range(len(dataset[0])-1):
+        str_column_to_float(dataset, i)
+        
+    # Convert class column to ints
+    str_column_to_int(dataset, len(dataset[0])-1)
+    
+    # Evaluate algorithm
+
+    scores = evaluate_algorithm(dataset, k_nearest_neighbors, n_folds, num_neighbors, distance_func)
+    print('Scores %s' % scores)
+    print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
     
 '''
 Execute main function
@@ -151,4 +211,5 @@ Execute main function
 if __name__ == "__main__":
     #test_dist_funcs()
     #test_neighbors()
-    test_prediction()
+    #test_prediction()
+    kNN_driver()
